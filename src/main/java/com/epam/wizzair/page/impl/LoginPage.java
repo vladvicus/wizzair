534dc19a1ce1fdc6263cf45bcdb7e88c651dcb01
ivan_surus <Ivan@sklbook.mks.local>
2017-03-09 13:05:47 +0300

    selectSeatPage and loginPage

---
+++ src/main/java/com/epam/wizzair/page/impl/LoginPage.java
@@ -0,0 +1,45 @@
+package com.epam.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class LoginPage extends AbstractPage {
+
+    private final String BASE_URL = "https://wizzair.com/";
+
+    @FindBy(xpath = "//ul[@class='navigation__container']/li[3]")
+    private WebElement signInButton;
+
+    @FindBy(xpath = "//form[@name='login-form']//input[@type='email']")
+    private WebElement emailField;
+
+
+    @FindBy(xpath = "//form[@name='login-form']//input[@type='password']")
+    private WebElement passwordField;
+
+
+    @FindBy(xpath = "//form[@name='login-form']//button[@type='submit']")
+    private WebElement submitSingInButton;
+
+
+
+    public LoginPage(WebDriver driver){
+        super(driver);
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @Override
+    public void openPage() {
+        driver.navigate().to(BASE_URL);
+    }
+
+    public void login(String email, String password){
+        signInButton.click();
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        submitSingInButton.click();
+    }
+}
