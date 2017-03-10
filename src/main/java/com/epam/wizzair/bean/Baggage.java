ec951ea8535a6e417addda7b9cb7f3c6a98e5090
Dzmitry_Sankouski <Dzmitry_Sankouski>
2017-03-10 14:21:37 +0300

    beans for passenger Page

---
+++ src/main/java/com/epam/wizzair/bean/Baggage.java
@@ -0,0 +1,43 @@
+package com.epam.wizzair.bean;
+
+import com.epam.wizzair.page.util.BaggageCabinOptions;
+import com.epam.wizzair.page.util.BaggageCheckedOptions;
+
+/**
+ * Created by Dzmitry_Sankouski on 10-Mar-17.
+ */
+class Baggage {
+    BaggageCabinOptions cabinBaggage;
+    BaggageCheckedOptions checkedBaggage;
+    boolean isSportEquipment;
+
+    public Baggage(){
+
+    }
+
+    public BaggageCabinOptions getCabinBaggage() {
+        return cabinBaggage;
+    }
+
+    public void setCabinBaggage(BaggageCabinOptions cabinBaggage) {
+        this.cabinBaggage = cabinBaggage;
+    }
+
+    public BaggageCheckedOptions getCheckedBaggage() {
+        return checkedBaggage;
+    }
+
+    public void setCheckedBaggage(BaggageCheckedOptions checkedBaggage) {
+        this.checkedBaggage = checkedBaggage;
+    }
+
+    public boolean isSportEquipment() {
+        return isSportEquipment;
+    }
+
+    public void setSportEquipment(boolean sportEquipment) {
+        isSportEquipment = sportEquipment;
+    }
+
+    //todo equals&hashcode&toString
+}
---
+++ src/main/java/com/epam/wizzair/bean/PassengerData.java
@@ -0,0 +1,78 @@
+package com.epam.wizzair.bean;
+
+import com.epam.wizzair.page.util.CheckInMethod;
+
+/**
+ * Created by Dzmitry_Sankouski on 10-Mar-17.
+ */
+public class PassengerData {
+    String name;
+    String surName;
+    boolean isMaleGender;
+    CheckInMethod depCheckinMethod;
+    CheckInMethod retCheckinMethod;
+    Baggage depBaggage;
+    Baggage retBaggage;
+
+    public PassengerData(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public boolean isMaleGender() {
+        return isMaleGender;
+    }
+
+    public void setMaleGender(boolean maleGender) {
+        isMaleGender = maleGender;
+    }
+
+    public CheckInMethod getDepCheckinMethod() {
+        return depCheckinMethod;
+    }
+
+    public void setDepCheckinMethod(CheckInMethod depCheckinMethod) {
+        this.depCheckinMethod = depCheckinMethod;
+    }
+
+    public CheckInMethod getRetCheckinMethod() {
+        return retCheckinMethod;
+    }
+
+    public void setRetCheckinMethod(CheckInMethod retCheckinMethod) {
+        this.retCheckinMethod = retCheckinMethod;
+    }
+
+    public Baggage getDepBaggage() {
+        return depBaggage;
+    }
+
+    public void setDepBaggage(Baggage depBaggage) {
+        this.depBaggage = depBaggage;
+    }
+
+    public Baggage getRetBaggage() {
+        return retBaggage;
+    }
+
+    public void setRetBaggage(Baggage retBaggage) {
+        this.retBaggage = retBaggage;
+    }
+
+    //todo equals&hashcode&toString
+}
