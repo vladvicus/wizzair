5417774135e0dd6d0419fbd28b1061e00e149d10
Dzmitry_Sankouski <Dzmitry_Sankouski>
2017-03-13 17:50:56 +0300

    deleted initBrowser , closeDriver method , deleted Driver

--- src/main/java/com/epam/wizzair/step/impl/StepsForLoginPage.java
+++ src/main/java/com/epam/wizzair/step/impl/StepsForLoginPage.java
@@ -9,29 +9,14 @@ import org.apache.logging.log4j.Logger;
 
 public class StepsForLoginPage {
 
-    private WebDriver driver;
-
     private final Logger logger = LogManager.getRootLogger();
-
-    public void initBrowser()
-    {
-        driver = DriverSingleton.getDriver();
-    }
-
-    public void closeDriver()
-    {
-        driver.quit();
-    }
 
     public void loginWizzAir(String email, String password)
     {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.openPage();
-        loginPage.openSignInForm();
+        LoginPage loginPage = new LoginPage();
+//        loginPage.openPage();
+//        loginPage.openSignInForm();
         loginPage.login(email, password);
     }
-
-
-
 
 }
