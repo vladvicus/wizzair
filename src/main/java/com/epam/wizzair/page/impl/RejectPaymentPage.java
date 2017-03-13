87d5ac1cae814d93e5ac4cb937540b8a5590dab9
Aliaksandr_Krutsko <aliaksandr_krutsko@epam.com>
2017-03-13 12:10:51 +0300

    reject payment page

---
+++ src/main/java/com/epam/wizzair/page/impl/RejectPaymentPage.java
@@ -0,0 +1,34 @@
+package com.epam.wizzair.page.impl;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.epam.wizzair.driver.DriverSingleton.getDriver;
+
+
+public class RejectPaymentPage extends AbstractPage {
+
+    private WebDriverWait wait = new WebDriverWait(getDriver(), 5, 1000);
+    @FindBy(xpath = "//p[contains(text(),'It seems your bank rejected the payment.')]")
+    private WebElement rejectMessage;
+
+
+    public String getRejectMessage() {
+
+        wait.until(ExpectedConditions.elementToBeClickable(rejectMessage));
+        return rejectMessage.getText();
+
+
+    }
+
+    public RejectPaymentPage() {
+       // PageFactory.initElements(getDriver(), this);
+    }
+
+    @Override
+    public void openPage() {
+
+    }
+}
