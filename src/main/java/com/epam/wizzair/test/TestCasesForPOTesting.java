f259dae2fb5a9c19606e0f052af2f34014513049
Aliaksandr_Krutsko <aliaksandr_krutsko@epam.com>
2017-03-14 14:25:12 +0300

    POTest which works just fine

--- src/main/java/com/epam/wizzair/test/TestCasesForPOTesting.java
+++ src/main/java/com/epam/wizzair/test/TestCasesForPOTesting.java
@@ -1,8 +1,10 @@
 package com.epam.wizzair.test;
 
+import com.epam.wizzair.bean.FlightData;
+import com.epam.wizzair.helper.Creator;
 import com.epam.wizzair.page.impl.MainPage;
 import com.epam.wizzair.page.impl.RejectPaymentPage;
-import com.epam.wizzair.step.impl.MainPageStepsForPOTesting;
+import com.epam.wizzair.step.impl.*;
 import junit.framework.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -15,49 +17,55 @@ import static org.testng.Assert.assertEquals;
 public class TestCasesForPOTesting {
 
 
+    MainPage mainPage = new MainPage();
+    StepsForMainPage stepsForMainPage = new StepsForMainPage();
 
-    private String origin = "Vilnius";
-    private String destination = "Tel-Aviv";
-    private int departureDay = 22;
-    private int returnDay = 28;
-    MainPageStepsForPOTesting mainPage = new MainPageStepsForPOTesting();
+
+//    private String origin = "Vilnius";
+//    private String destination = "Tel-Aviv";
+//    private int departureDay = 22;
+//    private int returnDay = 28;
+    MainPageStepsForPOTesting mainPageTest = new MainPageStepsForPOTesting();
 
     @BeforeTest
     public void setup(){
-        //open(Config.url());
-        MainPage mainPage = new MainPage();
+
         mainPage.openPage();
 
-
     }
-
 
 
     @Test
     public void flitsSum() throws InterruptedException {
-        mainPage.findFlight(origin, destination, departureDay, returnDay);
-        Assert.assertEquals(mainPage.getTwoFlightPrices(), mainPage.getFlightSumFromLeftWindow());
+        mainPageTest.signIn().loginWizzAir(Creator.getLogin());
+        //mainPageTest.findFlight(origin, destination, departureDay, returnDay);
+        stepsForMainPage.findFlight(new FlightData());
+        Assert.assertEquals(mainPageTest.getTwoFlightPrices(), mainPageTest.getFlightSumFromLeftWindow());
     }
+
+
+
 
     @Test
 
-    public void pay() throws InterruptedException {
-
-
+    public void payForBooking() {
+        String bean = "bean";
+        Creator.setPropertyFile(bean);
         MainPageStepsForPOTesting.getRidOfStickBar();
-        mainPage.signIn().loginWizzAir("tatester@12storage.com", "qwerty12345");
-        mainPage.findFlight(origin, destination, departureDay, returnDay);
-        MainPageStepsForPOTesting.getFlights();
-        MainPageStepsForPOTesting.choosePassengerEquipment();
-        MainPageStepsForPOTesting.continueFromSeats();
-        MainPageStepsForPOTesting.continueFromServicesPage();
-        MainPageStepsForPOTesting.declineWiz();
-        MainPageStepsForPOTesting.enterPayment();
+        stepsForMainPage.signIn().loginWizzAir(Creator.getLogin());
+        stepsForMainPage.findFlight(Creator.getFlightData()).getFlights().submit().fillBaggage(Creator.getBaggage()).submitAndGoToSeatSelection()
+                .continueFromSeats().submit().submit().continueToNextPage().fillBillingDetails(Creator.getBillingDetailsPersonal())
+                .fillCreditCard(Creator.getCreditCardDate()).submit();
         RejectPaymentPage rejectPaymentPage = new RejectPaymentPage();
         String message = rejectPaymentPage.getRejectMessage();
         assertEquals(message, "It seems your bank rejected the payment.");
 
+
     }
+
+
+
+
 
 
 
