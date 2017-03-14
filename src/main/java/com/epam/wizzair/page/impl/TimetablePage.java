8a43eee82068cf680585a76fbaedf681c95be496
Nadzeya_Parkhimovich <nadish@tut.by>
2017-03-14 14:35:28 +0300

    New TimeTable page, steps, test

--- src/main/java/com/epam/wizzair/page/impl/TimetablePage.java
+++ src/main/java/com/epam/wizzair/page/impl/TimetablePage.java
@@ -1,12 +1,15 @@
 package com.epam.wizzair.page.impl;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 import static com.epam.wizzair.driver.DriverSingleton.getDriver;
 
@@ -15,9 +18,11 @@ import static com.epam.wizzair.driver.DriverSingleton.getDriver;
  */
 public class TimetablePage extends AbstractPage {
 
-    private WebDriverWait wait = new WebDriverWait(getDriver(), 5, 1000);
+    private WebDriverWait wait = new WebDriverWait(getDriver(), 10, 1000);
     private final String URL = "https://wizzair.com/en-gb/information-and-services/destinations/timetable#/";
     private final String CITY = "//strong[text()='";
+    private final String FLIGHT_PATH = "//li[@class='fare-finder__calendar__days__day']";
+
 
     @FindBy(id= "search-departure-station")
     private WebElement inputOriginName;
@@ -27,6 +32,22 @@ public class TimetablePage extends AbstractPage {
 
     @FindBy(xpath = "//div[@class='fare-finder__sidebar__submit']/button[text()='Search']")
     private WebElement searchButton;
+
+    @FindBy(xpath = "//div[@class='fare-finder__calendar box']")
+    private List<WebElement> divFlights;
+
+    @FindBy(xpath = "//dd[@class='summary__price t__summary__price']/strong")
+    private WebElement summaryPrice;
+
+    @FindBy(xpath = "//div[@class='sidebar fare-finder__sidebar']/div[2]//strong[@class='fare-finder__sidebar__flight-card__content__price__amount']")
+    private WebElement firstFlightPrice;
+
+    @FindBy(xpath = "//div[@class='sidebar fare-finder__sidebar']/div[3]//strong[@class='fare-finder__sidebar__flight-card__content__price__amount']")
+    private WebElement secondFlightPrice;
+
+    @FindBy(xpath = "//div[@class='sidebar fare-finder__sidebar']/button")
+    private WebElement bookingButton;
+
 
     public TimetablePage(){
 
@@ -60,5 +81,45 @@ public class TimetablePage extends AbstractPage {
         wait.until(ExpectedConditions.elementToBeClickable(searchButton));
         searchButton.click();
         return this;
+    }
+
+    public TimetablePage chooseFirstFlight() {
+        WebElement flights = divFlights.get(0);
+        List<WebElement> firstFlight = flights.findElements(By.xpath(FLIGHT_PATH));
+        WebElement el = firstFlight.get(0);
+        el.click();
+        return this;
+    }
+
+    public TimetablePage chooseSecondFlight() {
+        ((JavascriptExecutor)getDriver()).executeScript("window.scrollBy(0,7000);");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement flights = divFlights.get(1);
+        List<WebElement> secondFlight = flights.findElements(By.xpath(FLIGHT_PATH));
+        WebElement el = secondFlight.get(secondFlight.size()-1);
+        el.click();
+        return this;
+    }
+
+    public String getSummaryPrice() {
+        return summaryPrice.getText();
+    }
+
+    public String getFirstFlightPrice() {
+        return firstFlightPrice.getText();
+    }
+
+    public String getSecondFlightPrice() {
+        return secondFlightPrice.getText();
+    }
+
+    public SearchResult startBooking() {
+        wait.until(ExpectedConditions.elementToBeClickable(bookingButton));
+        bookingButton.click();
+        return new SearchResult();
     }
 }
