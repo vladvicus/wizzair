90b9b383124c25bf57ce8ef0ae959002a4a1983d
Aliaksandr_Krutsko <aliaksandr_krutsko@epam.com>
2017-03-14 14:24:16 +0300

    method added for POTest to work

--- src/main/java/com/epam/wizzair/step/impl/StepsForSearchResult.java
+++ src/main/java/com/epam/wizzair/step/impl/StepsForSearchResult.java
@@ -1,7 +1,32 @@
 package com.epam.wizzair.step.impl;
 
+
+import com.epam.wizzair.page.impl.SearchResult;
+
 /**
  * Created by Dzmitry_Sankouski on 13-Mar-17.
  */
 public class StepsForSearchResult {
+
+    public StepsForSearchResult getFlights() {
+
+        SearchResult searchResult = new SearchResult();
+
+      searchResult.chooseFirstFlight();
+      searchResult.chooseSecondFlight();
+
+        return this;
+    }
+
+    public PassengerSteps submit() {
+        SearchResult searchResult = new SearchResult();
+        searchResult.continueToNextPage();
+        return new PassengerSteps();
+
+
+    }
+
+
+
+
 }
