3ce16bcb555e2890e09cbe767f214d9f6cc3dbc2
Dzmitry_Sankouski <Dzmitry_Sankouski>
2017-03-14 16:37:18 +0300

    update

--- src/main/java/com/epam/wizzair/test/TestSuite.java
+++ src/main/java/com/epam/wizzair/test/TestSuite.java
@@ -1,13 +1,17 @@
 package com.epam.wizzair.test;
 
+import com.epam.wizzair.bean.Baggage;
 import com.epam.wizzair.bean.FlightData;
+import com.epam.wizzair.bean.PassengerData;
 import com.epam.wizzair.driver.DriverSingleton;
-import com.epam.wizzair.page.impl.MainPage;
+import com.epam.wizzair.helper.Creator;
+import com.epam.wizzair.page.util.BaggageCabinOptions;
+import com.epam.wizzair.page.util.BaggageCheckedOptions;
+import com.epam.wizzair.page.util.CheckInMethod;
 import com.epam.wizzair.step.impl.StepsForMainPage;
 import com.epam.wizzair.step.impl.StepsForSearchResult;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -16,22 +20,92 @@ import org.testng.annotations.Test;
 public class TestSuite {
     StepsForMainPage mainSteps = new StepsForMainPage();
 
-
+//----------TestCase id=2
     @Test
     public void summingTwoFlights(){
-        FlightData flightData = new FlightData();
-
-        flightData.setDepDate(22);
-        flightData.setRetDate(28);
-        flightData.setOrigin("Vilnius");
-        flightData.setDestination("Tel-Aviv");
-        //todo retirieve this obj from somewhere twickable
+        FlightData flightData = Creator.getFlightData();
 
         StepsForSearchResult result;
         result = mainSteps.init()
                 .findFlight(flightData)
                 .pickExactFlights();
         Assert.assertEquals(result.getTwoFlightPrices(), result.getFlightSumFromLeftWindow());
+    }
+
+    //----------TestCase id=4
+    @Test
+    public void baggageCheck(){
+        FlightData flightData = Creator.getFlightData();
+
+        PassengerData passengerData = new PassengerData();
+
+//        Baggage depBaggage = new Baggage();
+//        depBaggage.setCabinBaggage(BaggageCabinOptions.LARGE);
+//        depBaggage.setCheckedBaggage(BaggageCheckedOptions.LIGHT);
+//        depBaggage.setSportEquipment(true);
+//
+//        Baggage retBaggage = new Baggage();
+//
+//        depBaggage.setCabinBaggage(BaggageCabinOptions.LARGE);
+//        depBaggage.setCheckedBaggage(BaggageCheckedOptions.LIGHT);
+//        depBaggage.setSportEquipment(true);
+//
+//        passengerData.setDepBaggage(depBaggage);
+//        passengerData.setRetBaggage(retBaggage);
+//        passengerData.setDepCheckinMethod(CheckInMethod.AIRPORT);
+//        passengerData.setRetCheckinMethod(CheckInMethod.AIRPORT);
+//        passengerData.setMaleGender(true);
+//        passengerData.setName("khsdfge");
+//        passengerData.setSurName("uiuijk");
+//        //todo retirieve this obj from somewhere twickable
+
+        passengerData = Creator.getPassengerData();
+
+        StepsForSearchResult result;
+        mainSteps.init().closePopUps()
+                .findFlight(flightData)
+                .pickExactFlights().submit()
+                .fillPassenger(passengerData)
+
+        ;
+//        Assert.assertEquals(result.getTwoFlightPrices(), result.getFlightSumFromLeftWindow());
+    }//todo complete Info column Page, & finish asserts
+
+    //----------TestCase id=5
+    @Test
+    public void bookingWrongCard(){
+        FlightData flightData = Creator.getFlightData();
+        PassengerData passengerData = Creator.getPassengerData();
+
+//        Baggage depBaggage = new Baggage();
+//        depBaggage.setCabinBaggage(BaggageCabinOptions.LARGE);
+//        depBaggage.setCheckedBaggage(BaggageCheckedOptions.LIGHT);
+//        depBaggage.setSportEquipment(true);
+//
+//        Baggage retBaggage = new Baggage();
+//
+//        depBaggage.setCabinBaggage(BaggageCabinOptions.LARGE);
+//        depBaggage.setCheckedBaggage(BaggageCheckedOptions.LIGHT);
+//        depBaggage.setSportEquipment(true);
+//
+//        passengerData.setDepBaggage(depBaggage);
+//        passengerData.setRetBaggage(retBaggage);
+//        passengerData.setDepCheckinMethod(CheckInMethod.AIRPORT);
+//        passengerData.setRetCheckinMethod(CheckInMethod.AIRPORT);
+//        passengerData.setMaleGender(true);
+//        passengerData.setName("khsdfge");
+//        passengerData.setSurName("uiuijk");
+//        //todo retirieve this obj from somewhere twickable
+
+
+        StepsForSearchResult result;
+        mainSteps.init().closePopUps()
+                .findFlight(flightData)
+                .pickExactFlights().submit()
+                .fillPassenger(passengerData)
+
+        ;
+//        Assert.assertEquals(result.getTwoFlightPrices(), result.getFlightSumFromLeftWindow());
     }
 
     @AfterClass
