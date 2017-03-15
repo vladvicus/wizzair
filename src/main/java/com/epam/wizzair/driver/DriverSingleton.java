8d82dcb07100eaacfed7335a8843c62d80eb6e68
Dzmitry_Sankouski <Dzmitry_Sankouski>
2017-03-15 13:05:28 +0300

    added openNewWindow & closeWindow methods

--- src/main/java/com/epam/wizzair/driver/DriverSingleton.java
+++ src/main/java/com/epam/wizzair/driver/DriverSingleton.java
@@ -1,18 +1,28 @@
 package com.epam.wizzair.driver;
 import com.epam.wizzair.helper.Config;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.concurrent.TimeUnit;
 
 public class DriverSingleton {
 
 
 
+
     private static String driverType = Config.browser();
 
     private static WebDriver driver = null;
+
+    private static Deque<String> windowStack = new LinkedList<>();
 
     private DriverSingleton() {
 
@@ -42,6 +52,7 @@ public class DriverSingleton {
 
             }
             driver.manage().window().maximize();
+            windowStack.push(driver.getWindowHandle());
             driver.manage().timeouts().implicitlyWait(Config.timeout(), TimeUnit.SECONDS);
             driver.manage().timeouts().pageLoadTimeout(Config.timeout(), TimeUnit.SECONDS);
             driver.manage().timeouts().setScriptTimeout(Config.timeout(), TimeUnit.SECONDS);
@@ -51,7 +62,23 @@ public class DriverSingleton {
 
 
 
+    public static void openNewWindow(){
+        WebElement body = driver.findElement(By.tagName("body"));
+        body.sendKeys(Keys.CONTROL + "n");
 
+        for(String winHandle : driver.getWindowHandles()){
+            driver.switchTo().window(winHandle);
+        }
+
+        windowStack.push(driver.getWindowHandle());
+    }
+
+    public static void closeWidow(){
+        WebElement body = driver.findElement(By.tagName("body"));
+        body.sendKeys(Keys.ALT, Keys.F4);
+        windowStack.poll();
+        driver.switchTo().window(windowStack.peek());
+    }
 
     public static void open(String url){
         getDriver().get(url);
